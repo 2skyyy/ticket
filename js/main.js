e98f09ab96a0ee0fe5034f38a2d86167062ec792
2skyyy <128768622+2skyyy@users.noreply.github.com>
2023-05-17 14:46:20 +0900

    popup창 구현

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+const popUp = document.querySelector('.popUp');
+const popCheckBox = document.querySelector('#popCheckBox');
+const closebtn = document.querySelector('.closebtn');
+
+const popUpDelete = function(){
+    popUp.style.display = "none";
+}
+
+popCheckBox.addEventListener('click', ()=>{
+    popUpDelete();
+});
+
+closebtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    popUpDelete();
+});
+
+
+
 $('.slider').slick({
     dots: true,
     arrows: true,
