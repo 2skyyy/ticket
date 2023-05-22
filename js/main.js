b711a9176f7eec67836ca560afe97f28e9ca749a
2skyyy <128768622+2skyyy@users.noreply.github.com>
2023-05-22 16:43:12 +0900

    리뷰창 댓글 구현

--- js/main.js
+++ js/main.js
@@ -41,20 +41,79 @@ window.addEventListener('scroll', function () {
 })
 
 
+//리뷰창 start
 const inputText = document.querySelector('.inputText');
 const plusBtn = document.querySelector('.plusBtn');
-const newLi = document.querySelector('.newLi');
 const listBoxUl = document.querySelector('.listBoxUl');
-const newLiEl = document.createElement('li');
+const reviewSelect = document.querySelector('#reviewSelect');
 
+const newN = () => {
+    const newLi = document.createElement('li');
+    newLi.classList.add('newLi');
 
+    const proflie = document.createElement('div');
+    proflie.classList.add('proflie');
+    const pro = document.createElement('a');
+    pro.classList.add('pro');
+    pro.textContent = "프"
+    const nickName = document.createElement('span');
+    nickName.classList.add('nickName');
+    nickName.textContent = "js25"
+    proflie.appendChild(pro);
+    proflie.appendChild(nickName);
+
+    const likeBtn = document.createElement('div');
+    likeBtn.classList.add('likeBtn');
+    const like = document.createElement('button');
+    like.classList.add('like');
+    like.textContent = "좋아요";
+    const hate = document.createElement('button');
+    hate.classList.add('hate');
+    hate.textContent = "싫어요";
+    likeBtn.appendChild(like);
+    likeBtn.appendChild(hate);
+
+    const revTxt = document.createElement('div');
+    revTxt.classList.add('revTxt');
+
+    const conName = document.createElement('div');
+    conName.classList.add('conName');
+    conName.textContent = "관람 공연: " + reviewSelect.options[reviewSelect.selectedIndex].text;
+    const conrev = document.createElement('div');
+    conrev.classList.add('conrev');
+    conrev.textContent = inputText.value;
+    inputText.value = "";
+    revTxt.appendChild(conName);
+    revTxt.appendChild(conrev);
+
+    const etc = document.createElement('div');
+    etc.classList.add('etc');
+    const date = document.createElement('span');
+    date.classList.add('date');
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth()+1;
+    let day = now.getDate();
+    date.textContent = (year + "." + month + "." + day);
+    const declaration = document.createElement('span');
+    declaration.classList.add('declaration');
+    declaration.textContent = "신고하기";
+    etc.appendChild(date);
+    etc.appendChild(declaration);
+
+    newLi.appendChild(proflie);
+    newLi.appendChild(likeBtn);
+    newLi.appendChild(revTxt);
+    newLi.appendChild(etc);
+    listBoxUl.appendChild(newLi);
+}
 
 const newList = () => {
-    
-    if(inputText.value.trim() != ""){
-        listBoxUl.appendChild(newLiEl);
-        newLiEl.classList.add('newLi');
-        inputText.value = "";
+    if(reviewSelect.options[reviewSelect.selectedIndex].value === ""){
+        alert("리뷰 남기실 공연을 선택해주세요")
+    }
+    if(inputText.value.trim() !== "" && reviewSelect.options[reviewSelect.selectedIndex].value != "") {
+        newN();
     }
 }
 
@@ -67,3 +126,8 @@ const enter = () => {
         newList();
     }
 }
+
+declaration.addEventListener('click', ()=>{
+    prompt("신고 사유를 적어주세요")
+})
+//리뷰창 end
